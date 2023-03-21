daf07f3cacc37a3eb8f4c7fc35951ea8f8d63a4a
AlekseyVereshchahin <amaverik281@gmail.com>
2023-03-21 23:01:14 +0200

    h

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import manager.HelperUser;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,6 +21,7 @@ public class LoginTests extends TestBase{
         app.getHelperUser().fillLoginForm("amaverik281@gmail.com","Aaaaaaa1!");
         app.getHelperUser().submitLogin();
         Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+        app.getHelperUser().closeWindow();
 
     }
     @Test
@@ -27,22 +30,42 @@ public class LoginTests extends TestBase{
         app.getHelperUser().fillLoginForm("amaverik281@gmail.com","Aaaaaaa1!");
         app.getHelperUser().submitLogin();
         Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
-
-
-    }
-
-//    @Test
-//    public void loginWrongEmail(){
-//
-//    }
-//
-//    @Test
-//    public void loginWrongPassword(){
-//
-//    }
-
-    @AfterMethod
-    public void postCondition(){
         app.getHelperUser().closeWindow();
+
+
     }
+
+    @Test
+    public void loginWrongEmail(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("amaverik281gmail.com","Aaaaaaa1!");
+        Assert.assertTrue(app.getHelperUser().isElementPresent(By.xpath("//*[contains(text(),'look like email')]")));
+
+
+    }
+
+    @Test
+    public void loginWrongPassword(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("amaverik281@gmail.com","Aaaaaaa1");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isElementPresent(By.xpath("//h2[contains(text(),'Login or Password incorrect')]")));
+        app.getHelperUser().closeWindow();
+
+    }
+
+    @Test
+    public void loginUnregisteredUser(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("pop@gmail.com","PopAaaaaaaA1!");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isElementPresent(By.xpath("//h2[contains(text(),'Login or Password incorrect')]")));
+        app.getHelperUser().closeWindow();
+
+    }
+
+//    @AfterMethod
+//    public void postCondition(){
+//        app.getHelperUser().closeWindow();
+//    }
 }
